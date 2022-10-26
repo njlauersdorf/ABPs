6028a0b784c9c229800aa4f023c463c2811d7b67
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 16:05:27 -0400

    testing

--- ABPs/run_sim/run_gpu.sh
+++ ABPs/run_sim/run_gpu.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash                    
 #SBATCH --partition=gpu
 #SBATCH --qos gpu_access                    # quality of service
 #SBATCH --gres=gpu:1                        # I want one gpus
@@ -6,27 +6,15 @@
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
 #SBATCH --constraint=rhel8
 #SBATCH --exclude=g0605
-# Don't run this for now SBATCH --exclude=g0605
-
-echo test
-echo $SHELL
-
 source ~/.bashrc
 
-echo test2
-echo $SHELL
-
+# Don't run this for now SBATCH --exclude=g0605
 module load gcc/9.1.0
 module load cuda/11.4
 
-echo test3
-echo $SHELL
+#source ~/virtual_envs/rekt/bin/activate
 
 conda activate rekt
 
-echo test4
-echo $SHELL
-
 filename=$1
-
 python3 $filename --mode=gpu # I want one gpu
