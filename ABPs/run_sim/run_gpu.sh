0047a3003e62cd568ab6ee0c332bab587a97ae04
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 15:26:49 -0400

    changed shell of run_gpu.sh

--- ABPs/run_sim/run_gpu.sh
+++ ABPs/run_sim/run_gpu.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#!/bin/bash
+#SBATCH --partition=gpu
 #SBATCH --qos gpu_access                    # quality of service
 #SBATCH --gres=gpu:1                        # I want one gpus
-#SBATCH --partition=gpu
 #SBATCH --nodes=1                     # partition to run on
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
+#SBATCH --constraint=rhel8
 #SBATCH --exclude=g0605
 # Don't run this for now SBATCH --exclude=g0605
-#source ~/.bashrc
+source ~/.bashrc
 
 module load gcc/9.1.0
 module load cuda/11.4
 
-conda init bash
 conda activate rekt
 
 filename=$1
