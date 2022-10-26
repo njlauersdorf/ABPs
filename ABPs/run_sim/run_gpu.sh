c56d5b1593f164b5ce53ce1a47605183b21d3223
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 14:52:04 -0400

    updated bash script for running on longleaf

--- ABPs/run_sim/run_gpu.sh
+++ ABPs/run_sim/run_gpu.sh
@@ -6,7 +6,13 @@
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
 #SBATCH --exclude=g0605
 # Don't run this for now SBATCH --exclude=g0605
+source ~/.bashrc
+
+module load gcc/9.1.0
+module load cuda/11.4
+
+conda activate rekt
 
 filename=$1
 
-python3.5 $filename --mode=gpu # I want one gpu
+python3 $filename --mode=gpu # I want one gpu
