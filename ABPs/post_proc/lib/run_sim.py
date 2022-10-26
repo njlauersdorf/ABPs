b321807eef0ecaa2b8c40197535eb0d01aa3c248
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 16:59:54 -0400

    testing

--- ABPs/post_proc/lib/run_sim.py
+++ ABPs/post_proc/lib/run_sim.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Initial imports
 import sys
 import os
--- ABPs/runPeloopBinaryCluster_new.sh
+++ ABPs/runPeloopBinaryCluster_new.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+source ~/miniconda3/etc/profile.d/conda.sh
+
+source ~/.bashrc
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate rekt
+
 current=$( date "+%m_%d_%y" )
 this_path=$( pwd )
 
