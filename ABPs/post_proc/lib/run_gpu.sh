0dd4574384b9b677529f099e3c0c4b251c8f76af
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 11:50:44 -0400

    modified commands needed for gpu for running simulations

--- ABPs/post_proc/lib/run_gpu.sh
+++ ABPs/post_proc/lib/run_gpu.sh
@@ -7,6 +7,11 @@
 #SBATCH --exclude=g0605
 # Don't run this for now SBATCH --exclude=g0605
 
+module load gcc/9.1.0
+module load cuda/11.4
+
+conda activate rekt
+
 filename=$1
 
-python3.5 $filename --mode=gpu # I want one gpu
+python3 $filename --mode=gpu # I want one gpu
