35b6733044e11a60b90d9e80c911dcf7cdea7a9b
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 17:08:56 -0400

    testing

--- ABPs/run_sim/run_gpu.sh
+++ ABPs/run_sim/run_gpu.sh
@@ -6,19 +6,16 @@
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
 #SBATCH --constraint=rhel8
 #SBATCH --exclude=g0605
-source ~/miniconda3/etc/profile.d/conda.sh
-
+#source ~/miniconda3/etc/profile.d/conda.sh
+eval "$(conda shell.bash hook)"
 source ~/.bashrc
 
-conda deactivate
 # Don't run this for now SBATCH --exclude=g0605
 module load gcc/9.1.0
 module load cuda/11.4
 
-source ~/miniconda3/etc/profile.d/conda.sh
-conda deactivate
+eval "$(conda shell.bash hook)"
 conda activate rekt
-conda deactivate
 
 filename=$1
 python3 $filename --mode=gpu # I want one gpu
