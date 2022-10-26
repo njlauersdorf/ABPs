d50104d19774c0f11803d7e19ea5bab831b7a1f8
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-10-26 15:18:13 -0400

    removed bashrc line

--- ABPs/run_sim/run_gpu.sh
+++ ABPs/run_sim/run_gpu.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
 #SBATCH --exclude=g0605
 # Don't run this for now SBATCH --exclude=g0605
-source ~/.bashrc
+#source ~/.bashrc
 
 module load gcc/9.1.0
 module load cuda/11.4
