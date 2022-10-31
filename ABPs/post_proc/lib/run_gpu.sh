6597d951ab1755c980bc0631a6124c4b1295bea0
Nicholas Jacob Lauersdorf <njlauers@longleaf-login5.its.unc.edu>
2022-10-31 14:15:48 -0400

    fixed run_gpu.sh

--- ABPs/post_proc/lib/run_gpu.sh
+++ ABPs/post_proc/lib/run_gpu.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#!/bin/bash                    
+#SBATCH --partition=gpu
 #SBATCH --qos gpu_access                    # quality of service
 #SBATCH --gres=gpu:1                        # I want one gpus
-#SBATCH --partition=gpu
 #SBATCH --nodes=1                     # partition to run on
 #SBATCH --time=11-00:00                     # time (D-HH:MM)
+#SBATCH --constraint=rhel8
 #SBATCH --exclude=g0605
-# Don't run this for now SBATCH --exclude=g0605
+source ~/.bashrc
 
+# Don't run this for now SBATCH --exclude=g0605
 module load gcc/9.1.0
 module load cuda/11.4
+
+#source ~/virtual_envs/rekt/bin/activate
 
 conda activate rekt
 
 filename=$1
-
 python3 $filename --mode=gpu # I want one gpu
