5b20eea69323c8a1901a878abe288ef0f30fb9a9
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2022-11-28 08:33:41 -0500

    changed simulation parameters

--- ABPs/runPeloopBinaryCluster_new.sh
+++ ABPs/runPeloopBinaryCluster_new.sh
@@ -135,7 +135,7 @@ if [ $dont_run == "no" ]; then
     # List for phi
     declare -a phi
     #phi=(60)
-    phi=(60 70 80 90 100 110 120)
+    phi=(60 70 80 90 110 120)
     # List for epsilon
     #eps=(1.0 0.1 0.001) # LISTS CAN CONTAIN FLOATS!!!!
     declare -a eps
