fixed orientation vector calculation

post_proc/full_density_analysis_binary_updates_temp.py
@@ -321,6 +321,8 @@ with hoomd.open(name=inFile, mode='rb') as t:
 
         ori = snap.particles.orientation            #current orientation (quaternions)
         ang = np.array(list(map(utility_functs.quatToAngle, ori))) # convert to [-pi, pi]
+        x_orient_arr = np.array(list(map(utility_functs.quatToXOrient, ori))) # convert to [-pi, pi]
+        y_orient_arr = np.array(list(map(utility_functs.quatToYOrient, ori))) # convert to [-pi, pi]
 
         typ = snap.particles.typeid                 # Particle type
         typ0ind=np.where(snap.particles.typeid==0)      # Calculate which particles are type 0
@@ -1142,13 +1144,13 @@ with hoomd.open(name=inFile, mode='rb') as t:
                     if plot == 'y':
                         
                         # Plot particles color-coded by activity
-                        plotting_functs.plot_part_activity_wide_adsorb_orient(pos, ang, all_surface_curves, int_comp_dict, active_fa_dict, mono_id = mono_option, zoom_id = zoom_option, interface_id = interface_option, orientation_id = orientation_option, banner_id = banner_option, presentation_id = presentation_option)
+                        plotting_functs.plot_part_activity_wide_adsorb_orient(pos, x_orient_arr, y_orient_arr, all_surface_curves, int_comp_dict, active_fa_dict, mono_id = mono_option, zoom_id = zoom_option, interface_id = interface_option, orientation_id = orientation_option, banner_id = banner_option, presentation_id = presentation_option)
                 elif (measurement_options[0] == 'activity-wide-desorb-orient'):
                     #DONE!
                     if plot == 'y':
                         
                         # Plot particles color-coded by activity
-                        plotting_functs.plot_part_activity_wide_desorb_orient(pos, ang, all_surface_curves, int_comp_dict, active_fa_dict, mono_id = mono_option, zoom_id = zoom_option, interface_id = interface_option, orientation_id = orientation_option, banner_id = banner_option, presentation_id = presentation_option)
+                        plotting_functs.plot_part_activity_wide_desorb_orient(pos, x_orient_arr, y_orient_arr, all_surface_curves, int_comp_dict, active_fa_dict, mono_id = mono_option, zoom_id = zoom_option, interface_id = interface_option, orientation_id = orientation_option, banner_id = banner_option, presentation_id = presentation_option)
 
 
                 elif measurement_options[0] == 'activity-com':
@@ -2258,6 +2260,8 @@ with hoomd.open(name=inFile, mode='rb') as t:
 
                 ori = snap.particles.orientation            #Orientation (quaternions)
                 ang = np.array(list(map(utility_functs.quatToAngle, ori))) # convert to [-pi, pi]
+                x_orient_arr = np.array(list(map(utility_functs.quatToXOrient, ori))) # convert to [-pi, pi]
+                y_orient_arr = np.array(list(map(utility_functs.quatToYOrient, ori))) # convert to [-pi, pi]
 
 
                 typ = snap.particles.typeid                 # Particle type

post_proc/lib/plotting.py
@@ -6914,8 +6914,8 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
 
         sz = 0.755
 
-        px = np.sin(ang)
-        py = -np.cos(ang)
+        px = np.cos(ang)
+        py = np.sin(ang)
 
         if mono==0:
 
@@ -6965,6 +6965,8 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                     fastGroup = mc.PatchCollection(ells0,facecolors=fastCol)
                 ax.add_collection(slowGroup)
                 ax.add_collection(fastGroup)
+
+                
 
                 plt.quiver((pos[:,0]+self.hx_box)-(np.sqrt(2)/2)*self.lx_box, (pos[:,1]+self.hy_box)-(np.sqrt(2)/2)*self.ly_box, px, py, scale=200.0, color='black', alpha=0.8)
                 plt.quiver((pos[:,0]+self.hx_box)-(np.sqrt(2)/2)*self.lx_box, (pos[:,1]+self.hy_box)+(np.sqrt(2)/2)*self.ly_box, px, py, scale=200.0, color='black', alpha=0.8)
@@ -7220,6 +7222,8 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         if orientation_id == True:
             try:
                 if active_fa_dict!=None:
+
+                    
                     plt.quiver(self.pos_x, self.pos_y, active_fa_dict['bin']['x'], active_fa_dict['bin']['y'], scale=20.0, color='black', alpha=0.8)
             except:
                 pass
@@ -7295,7 +7299,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         #plt.savefig(self.outPath + 'part_activity_' + self.outFile + ".eps", format='eps', dpi=150, bbox_inches='tight')
         plt.close()  
 
-    def plot_part_activity_wide_desorb_orient(self, pos, ang, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
+    def plot_part_activity_wide_desorb_orient(self, pos, px, py, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
 
         """
         This function plots the particle positions and color codes each particle with its intrinsic
@@ -7470,9 +7474,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
             pos[:,1]= np.sin(np.pi/4) * pos_old[:,0] + np.cos(np.pi/4) * pos_old[:,1]
 
         sz = 0.755
-
-        px = np.sin(ang)
-        py = -np.cos(ang)
 
         if mono==0:
 
@@ -8384,7 +8385,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         #plt.savefig(self.outPath + 'part_activity_' + self.outFile + ".eps", format='eps', dpi=150, bbox_inches='tight')
         plt.close()  
 
-    def plot_part_activity_wide_adsorb_orient(self, pos, ang, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
+    def plot_part_activity_wide_adsorb_orient(self, pos, px, py, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
 
         """
         This function plots the particle positions and color codes each particle with its intrinsic
@@ -8558,11 +8559,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
             pos[:,0]= np.cos(np.pi/4) * pos_old[:,0] - np.sin(np.pi/4) * pos_old[:,1]
             pos[:,1]= np.sin(np.pi/4) * pos_old[:,0] + np.cos(np.pi/4) * pos_old[:,1]
         
-        
-
         sz = 0.755
-        px = np.sin(ang)
-        py = -np.cos(ang)
 
         if mono==0:
 

post_proc/lib/utility.py
@@ -188,7 +188,45 @@ class utility:
                 return nBins
             else:
                 nBins -= 1
-
+    def quaternion_rotation_matrix(Q):
+        """
+        Covert a quaternion into a full three-dimensional rotation matrix.
+    
+        Input
+        :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
+    
+        Output
+        :return: A 3x3 element matrix representing the full 3D rotation matrix. 
+                This rotation matrix converts a point in the local reference 
+                frame to a point in the global reference frame.
+        """
+        # Extract the values from Q
+        q0 = Q[0]
+        q1 = Q[1]
+        q2 = Q[2]
+        q3 = Q[3]
+        
+        # First row of the rotation matrix
+        r00 = 2 * (q0 * q0 + q1 * q1) - 1
+        r01 = 2 * (q1 * q2 - q0 * q3)
+        r02 = 2 * (q1 * q3 + q0 * q2)
+        
+        # Second row of the rotation matrix
+        r10 = 2 * (q1 * q2 + q0 * q3)
+        r11 = 2 * (q0 * q0 + q2 * q2) - 1
+        r12 = 2 * (q2 * q3 - q0 * q1)
+        
+        # Third row of the rotation matrix
+        r20 = 2 * (q1 * q3 - q0 * q2)
+        r21 = 2 * (q2 * q3 + q0 * q1)
+        r22 = 2 * (q0 * q0 + q3 * q3) - 1
+        
+        # 3x3 rotation matrix
+        rot_matrix = np.array([[r00, r01, r02],
+                            [r10, r11, r12],
+                            [r20, r21, r22]])
+                                
+        return rot_matrix
     def quatToAngle(self, quat):
         '''
         Purpose: Take quaternion orientation vector of particle as given by hoomd-blue
@@ -205,9 +243,97 @@ class utility:
         x = quat[1]         #x-direction
         y = quat[2]         #y-direction
         z = quat[3]         #z-direction
-        rad = math.atan2(y, x)
 
-        return rad
+        rot_matrix = utility.quaternion_rotation_matrix(quat)
+        z_axis = np.array([0,0,1])
+        z_axis = np.reshape(z_axis, (3,1))
+
+        orientation_vector = np.matmul(rot_matrix, z_axis)
+        
+        rad = math.atan2(y, x)
+        rad2 = math.atan2(r, np.sqrt(x**2+y**2+z**2))
+        alpha = 2*np.arccos(r)
+        beta_x = np.arccos(x/np.sin(alpha/2))
+        beta_y = np.arccos(y/np.sin(alpha/2))
+        beta_z = np.arccos(z/np.sin(alpha/2))
+        phi = np.arctan2(2*(r*x + y*z),1-2*(x**2+y**2))
+        theta = -np.pi/2 + 2*np.arctan2(np.sqrt(1+2*(r*y-x*z)), np.sqrt(1-2*(r*y-x*z)))
+        psi = np.arctan2(2*(r*z + x*y),1-2*(y**2+z**2))
+        
+        x_vect = orientation_vector[0]
+        y_vect = orientation_vector[1]
+        #sto
+        
+        
+        #print(rad)
+        ##print(alpha)
+        #print(beta_x)
+        #print(beta_y)
+        #print(beta_z)
+        theta = np.arctan2(orientation_vector[1][0], orientation_vector[0][0])
+        #print((orientation_vector[1]**2 + orientation_vector[0]**2)**0.5 * np.cos(np.arctan2(orientation_vector[1], orientation_vector[0])))
+        #stop
+        
+
+        return theta#rad
+
+    def quatToXOrient(self, quat):
+        '''
+        Purpose: Take quaternion orientation vector of particle as given by hoomd-blue
+        simulations and output angle between [-pi, pi]
+
+        Inputs:
+        quat: Quaternion orientation vector of particle
+
+        Output:
+        rad: angle between [-pi, pi]
+        '''
+
+        r = quat[0]         #magnitude
+        x = quat[1]         #x-direction
+        y = quat[2]         #y-direction
+        z = quat[3]         #z-direction
+
+        rot_matrix = utility.quaternion_rotation_matrix(quat)
+        z_axis = np.array([0,0,1])
+        z_axis = np.reshape(z_axis, (3,1))
+
+        orientation_vector = np.matmul(rot_matrix, z_axis)
+
+        x_vect = orientation_vector[0]
+        y_vect = orientation_vector[1]
+        
+
+        return x_vect
+
+    def quatToYOrient(self, quat):
+        '''
+        Purpose: Take quaternion orientation vector of particle as given by hoomd-blue
+        simulations and output angle between [-pi, pi]
+
+        Inputs:
+        quat: Quaternion orientation vector of particle
+
+        Output:
+        rad: angle between [-pi, pi]
+        '''
+
+        r = quat[0]         #magnitude
+        x = quat[1]         #x-direction
+        y = quat[2]         #y-direction
+        z = quat[3]         #z-direction
+
+        rot_matrix = utility.quaternion_rotation_matrix(quat)
+        z_axis = np.array([0,0,1])
+        z_axis = np.reshape(z_axis, (3,1))
+
+        orientation_vector = np.matmul(rot_matrix, z_axis)
+
+        x_vect = orientation_vector[0]
+        y_vect = orientation_vector[1]
+        
+
+        return y_vect
 
     def symlog(self, x):
         """ Returns the symmetric log10 value """