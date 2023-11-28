fixed orientation calculation for all other calculations

post_proc/lib/binning.py
@@ -447,10 +447,7 @@ class binning:
         bulk_BB_q = 1.0 - (1/(len(bulk_B_id) * np.var(part_binned_arr['bulk']['B-B']['press']))) * bulk_BB_std_sum
         int_BB_q = 1.0 - (1/(len(int_B_id) * np.var(part_binned_arr['int']['B-B']['press']))) * int_BB_std_sum
         gas_BB_q = 1.0 - (1/(len(gas_B_id) * np.var(part_binned_arr['gas']['B-B']['press']))) * gas_BB_std_sum
-        
-        print((1/(len(gas_A_id) * np.var(part_binned_arr['gas']['all-A']['press']))))
-        print(gas_allA_std_sum)
-        print(gas_allA_q)
+
         q_dict = {'bulk': {'all-all': bulk_allall_q, 'all-A': bulk_allA_q, 'A-all': bulk_Aall_q, 'all-B': bulk_allB_q, 'B-all': bulk_Ball_q, 'A-A': bulk_AA_q, 'A-B': bulk_AB_q, 'B-A': bulk_BA_q, 'B-B': bulk_BB_q}, 'int': {'all-all': int_allall_q, 'all-A': int_allA_q, 'A-all': int_Aall_q, 'all-B': int_allB_q, 'B-all': int_Ball_q, 'A-A': int_AA_q, 'A-B': int_AB_q, 'B-A': int_BA_q, 'B-B': int_BB_q}, 'gas': {'all-all': gas_allall_q, 'all-A': gas_allA_q, 'A-all': gas_Aall_q, 'all-B': gas_allB_q, 'B-all': gas_Ball_q, 'A-A': gas_AA_q, 'A-B': gas_AB_q, 'B-A': gas_BA_q, 'B-B': gas_BB_q} }
         return q_dict
     def bin_parts_from_interpart_press(self, part_dict, press_hetero_dict):
@@ -1404,7 +1401,7 @@ class binning:
 
         return area_frac_dict
 
-    def bin_orient(self, part_dict, pos, ang, com):
+    def bin_orient(self, part_dict, pos, px, py, com):
         '''
         Purpose: Takes the particle ids and orientation, bins the particles,
         and calculates the average orientation of each bin
@@ -1487,30 +1484,26 @@ class binning:
 
                         difr = (difx ** 2 + dify ** 2) ** 0.5
 
-                        # x- and y-direction orientation unit vectors
-                        px = np.sin(ang[binParts[ix][iy][h]])
-                        py = -np.cos(ang[binParts[ix][iy][h]])
-
                         # Alignment toward largest cluster's CoM
-                        r_dot_p = -difx * px + -dify * py
+                        r_dot_p = -difx * px[binParts[ix][iy][h]] + -dify * py[binParts[ix][iy][h]]
 
                         # Sum orientation unit vectors in respective bin
-                        p_all_x[ix][iy] += px
-                        p_all_y[ix][iy] += py
+                        p_all_x[ix][iy] += px[binParts[ix][iy][h]]
+                        p_all_y[ix][iy] += py[binParts[ix][iy][h]]
 
                         if typParts[ix][iy][h] == 0:
                             typ0_temp += 1
-                            p_A_x[ix][iy] += px
-                            p_A_y[ix][iy] += py
+                            p_A_x[ix][iy] += px[binParts[ix][iy][h]]
+                            p_A_y[ix][iy] += py[binParts[ix][iy][h]]
 
                         elif typParts[ix][iy][h] == 1:
                             typ1_temp += 1
-                            p_B_x[ix][iy] += px
-                            p_B_y[ix][iy] += py
+                            p_B_x[ix][iy] += px[binParts[ix][iy][h]]
+                            p_B_y[ix][iy] += py[binParts[ix][iy][h]]
 
                         # Save particle orientation unit vectors
-                        bin_part_x[binParts[ix][iy][h]]=px
-                        bin_part_y[binParts[ix][iy][h]]=py
+                        bin_part_x[binParts[ix][iy][h]]=px[binParts[ix][iy][h]]
+                        bin_part_y[binParts[ix][iy][h]]=py[binParts[ix][iy][h]]
 
                     # Calculate average orientation unit vector per bin for each particle type ('all', 'A', or 'B')
                     p_avg_x[ix][iy] = p_all_x[ix][iy] / len(binParts[ix][iy])

post_proc/lib/interface.py
@@ -38,7 +38,7 @@ import utility
 
 # Class of interface identification functions
 class interface:
-    def __init__(self, area_frac_dict, align_dict, part_dict, press_dict, lx_box, ly_box, partNum, NBins_x, NBins_y, peA, peB, parFrac, eps, typ, ang):
+    def __init__(self, area_frac_dict, align_dict, part_dict, press_dict, lx_box, ly_box, partNum, NBins_x, NBins_y, peA, peB, parFrac, eps, typ, px, py):
 
         # Array (NBins_x, NBins_y) of average alignment of all particles per bin in either direction ('x', 'y', or 'mag'nitude)
         self.align_x = align_dict['bin']['all']['x']
@@ -140,7 +140,8 @@ class interface:
         self.typ = typ
 
         # Array (partNum) of particle orientations [-pi,pi]
-        self.ang = ang
+        self.px = px
+        self.py = py
 
     def det_surface_points(self, phase_dict, int_dict, int_comp_dict):
         '''
@@ -2040,38 +2041,35 @@ class interface:
                                 if len(self.binParts[ix][iy])>0:
 
                                     for h in range(0, len(self.binParts[ix][iy])):
-                                        #Calculate x and y orientation of active force
-                                        px = np.sin(self.ang[self.binParts[ix][iy][h]])
-                                        py = -np.cos(self.ang[self.binParts[ix][iy][h]])
 
                                         #Calculate alignment towards CoM
                                         if interior_exist == 1:
                                             if (difr_short == difr_bub) | ( exterior_radius<=interior_radius):
-                                                x_dot_p = (-x_norm_unitv * px)
-                                                y_dot_p = (-y_norm_unitv * py)
+                                                x_dot_p = (-x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                                y_dot_p = (-y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                                 r_dot_p = x_dot_p + y_dot_p
                                             else:
-                                                x_dot_p = (x_norm_unitv * px)
-                                                y_dot_p = (y_norm_unitv * py)
+                                                x_dot_p = (x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                                y_dot_p = (y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                                 r_dot_p = x_dot_p + y_dot_p
                                         else:
                                             if (difr_short == difr_bub):
-                                                x_dot_p = (-x_norm_unitv * px)
-                                                y_dot_p = (-y_norm_unitv * py)
+                                                x_dot_p = (-x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                                y_dot_p = (-y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                                 r_dot_p = x_dot_p + y_dot_p
                                             else:  
-                                                x_dot_p = (x_norm_unitv * px)
-                                                y_dot_p = (y_norm_unitv * py)
+                                                x_dot_p = (x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                                y_dot_p = (y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                                 r_dot_p = x_dot_p + y_dot_p
 
-                                        x_dot_p_trad = (-x_norm_unitv_trad * px)
-                                        y_dot_p_trad = (-y_norm_unitv_trad * py)
+                                        x_dot_p_trad = (-x_norm_unitv_trad * self.px[self.binParts[ix][iy][h]])
+                                        y_dot_p_trad = (-y_norm_unitv_trad * self.py[self.binParts[ix][iy][h]])
                                         r_dot_p_trad = x_dot_p_trad + y_dot_p_trad
                                         #Sum x,y orientation over each bin
 
                                         new_align[ix][iy] += r_dot_p
-                                        new_align_x[ix][iy] += px
-                                        new_align_y[ix][iy] += py
+                                        new_align_x[ix][iy] += self.px[self.binParts[ix][iy][h]]
+                                        new_align_y[ix][iy] += self.py[self.binParts[ix][iy][h]]
                                         new_align_num[ix][iy]+= 1
                                         new_align_trad[ix][iy] += r_dot_p_trad
                                         new_align_trad_x[ix][iy] += x_dot_p
@@ -2161,27 +2159,24 @@ class interface:
 
                                 if len(self.binParts[ix][iy])>0:
                                     for h in range(0, len(self.binParts[ix][iy])):
-
-                                        px = np.sin(self.ang[self.binParts[ix][iy][h]])
-                                        py = -np.cos(self.ang[self.binParts[ix][iy][h]])
                                         #Calculate alignment towards CoM
 
                                         if interior_exist == 1:
                                             if (difr_short == difr_bub) | ( exterior_radius>interior_radius):
-                                                x_dot_p = (-x_norm_unitv * px)
-                                                y_dot_p = (-y_norm_unitv * py)
+                                                x_dot_p = (-x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                                y_dot_p = (-y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                                 r_dot_p = x_dot_p + y_dot_p
                                             else:
-                                                x_dot_p = (x_norm_unitv * px)
-                                                y_dot_p = (y_norm_unitv * py)
+                                                x_dot_p = (x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                                y_dot_p = (y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                                 r_dot_p = x_dot_p + y_dot_p
                                         else:
-                                            x_dot_p = (x_norm_unitv * px)
-                                            y_dot_p = (y_norm_unitv * py)
+                                            x_dot_p = (x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                            y_dot_p = (y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                             r_dot_p = x_dot_p + y_dot_p
 
-                                        x_dot_p_trad = (-x_norm_unitv_trad * px)
-                                        y_dot_p_trad = (-y_norm_unitv_trad * py)
+                                        x_dot_p_trad = (-x_norm_unitv_trad * self.px[self.binParts[ix][iy][h]])
+                                        y_dot_p_trad = (-y_norm_unitv_trad * self.py[self.binParts[ix][iy][h]])
                                         r_dot_p_trad = x_dot_p_trad + y_dot_p_trad
 
 
@@ -2189,8 +2184,8 @@ class interface:
                                         new_align[ix][iy] += r_dot_p
                                         new_align_x[ix][iy] += x_dot_p
                                         new_align_y[ix][iy] += y_dot_p
-                                        new_align_x[ix][iy] += px
-                                        new_align_y[ix][iy] += py
+                                        new_align_x[ix][iy] += self.px[self.binParts[ix][iy][h]]
+                                        new_align_y[ix][iy] += self.py[self.binParts[ix][iy][h]]
                                         new_align_num[ix][iy]+= 1
                                         new_align_trad[ix][iy] += r_dot_p_trad
                                         new_align_trad_x[ix][iy] += x_dot_p_trad
@@ -2448,28 +2443,25 @@ class interface:
                             if len(self.binParts[ix][iy])>0:
                                 #Loop over all particles in bin
                                 for h in range(0, len(self.binParts[ix][iy])):
-                                    #Calculate x and y orientation of active force
-                                    px = np.sin(self.ang[self.binParts[ix][iy][h]])
-                                    py = -np.cos(self.ang[self.binParts[ix][iy][h]])
 
                                     #Calculate alignment of single particle with nearest surface
 
                                     if (interior_bin_short == 1) | (difr_short == difr_bub):
-                                        x_dot_p = (-x_norm_unitv * px)
-                                        y_dot_p = (-y_norm_unitv * py)
+                                        x_dot_p = (-x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                        y_dot_p = (-y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                     elif exterior_bin_short == 1:
-                                        x_dot_p = (x_norm_unitv * px)
-                                        y_dot_p = (y_norm_unitv * py)
+                                        x_dot_p = (x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                        y_dot_p = (y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                                     else:
-                                        x_dot_p = (-x_norm_unitv * px)
-                                        y_dot_p = (-y_norm_unitv * py)
+                                        x_dot_p = (-x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                        y_dot_p = (-y_norm_unitv * self.py[self.binParts[ix][iy][h]])
 
 
                                     r_dot_p = x_dot_p + y_dot_p
 
                                     #Calculate alignment of single particle with cluster's Center of mass
-                                    x_dot_p_trad = (-x_norm_unitv_trad * px)
-                                    y_dot_p_trad =  (-y_norm_unitv_trad * py)
+                                    x_dot_p_trad = (-x_norm_unitv_trad * self.px[self.binParts[ix][iy][h]])
+                                    y_dot_p_trad =  (-y_norm_unitv_trad * self.py[self.binParts[ix][iy][h]])
                                     r_dot_p_trad = x_dot_p_trad + y_dot_p_trad
 
                                     #Save alignment of each particle
@@ -2738,29 +2730,25 @@ class interface:
                         #Loop over particles in reference bin
                         for h in range(0, len(self.binParts[ix][iy])):
 
-                            #Calculate x and y orientation of reference particle's active force
-                            px = np.sin(self.ang[self.binParts[ix][iy][h]])
-                            py = -np.cos(self.ang[self.binParts[ix][iy][h]])
-
                             #If nearest surface is exterior surface, calculate alignment with that surface
                             if exterior_bin_short == 1:
-                                Surface_x_dot_p = (-Surface_x_norm_unitv * px)
-                                Surface_y_dot_p = (-Surface_y_norm_unitv * py)
+                                Surface_x_dot_p = (-Surface_x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                Surface_y_dot_p = (-Surface_y_norm_unitv * self.py[self.binParts[ix][iy][h]])
 
                             #If nearest surface is interior surface, calculate alignment with that surface
                             elif interior_bin_short == 1:
-                                Surface_x_dot_p = (Surface_x_norm_unitv * px)
-                                Surface_y_dot_p = (Surface_y_norm_unitv * px)
+                                Surface_x_dot_p = (Surface_x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                Surface_y_dot_p = (Surface_y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                             else:
-                                Surface_x_dot_p = (-CoM_x_norm_unitv * px)
-                                Surface_y_dot_p = (-CoM_y_norm_unitv * py)
+                                Surface_x_dot_p = (-CoM_x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                                Surface_y_dot_p = (-CoM_y_norm_unitv * self.py[self.binParts[ix][iy][h]])
 
                             # Total alignment with nearest surface
                             Surface_r_dot_p = Surface_x_dot_p + Surface_y_dot_p
 
                             #Calculate alignment towards largest cluster's CoM (or middle of simulation box if no cluster present)
-                            CoM_x_dot_p = (-CoM_x_norm_unitv * px)
-                            CoM_y_dot_p = (-CoM_y_norm_unitv * py)
+                            CoM_x_dot_p = (-CoM_x_norm_unitv * self.px[self.binParts[ix][iy][h]])
+                            CoM_y_dot_p = (-CoM_y_norm_unitv * self.py[self.binParts[ix][iy][h]])
                             CoM_r_dot_p= CoM_x_dot_p + CoM_y_dot_p
 
                             # Save alignment of particle with nearest surface

post_proc/lib/particles.py
@@ -30,7 +30,7 @@ import theory, utility, plotting_utility
 # Class of individual particle property measurements
 class particle_props:
 
-    def __init__(self, lx_box, ly_box, partNum, NBins_x, NBins_y, peA, peB, eps, typ, pos, ang):
+    def __init__(self, lx_box, ly_box, partNum, NBins_x, NBins_y, peA, peB, eps, typ, pos, px, py):
 
         # Initialize theory functions for call back later
         theory_functs = theory.theory()
@@ -89,7 +89,8 @@ class particle_props:
         self.pos = pos
 
         # Array (partNum) of particle orientations
-        self.ang = ang
+        self.px = px
+        self.py = py
 
         # Initialize theory functions for call back later
         self.theory_functs = theory.theory()
@@ -173,8 +174,8 @@ class particle_props:
             y_unitv = (dify) / difr
 
             #Calculate x and y orientation of active force
-            px = np.sin(self.ang[h])
-            py = -np.cos(self.ang[h])
+            px = self.px[h]
+            py = self.py[h]
 
             #Calculate alignment towards CoM
             r_dot_p = (-x_unitv * px) + (-y_unitv * py)
@@ -219,8 +220,8 @@ class particle_props:
             y_unitv = (dify) / difr
 
             #Calculate x and y orientation of active force
-            px = np.sin(self.ang[h])
-            py = -np.cos(self.ang[h])
+            px = self.px[h]
+            py = self.py[h]
 
             #Calculate alignment towards CoM
             r_dot_p = (-x_unitv * px) + (-y_unitv * py)
@@ -400,8 +401,8 @@ class particle_props:
             y_norm_unitv = (dify) / difr
 
             #Calculate x and y orientation of active force
-            px = np.sin(self.ang[h])
-            py = -np.cos(self.ang[h])
+            px = self.px[h]
+            py = self.py[h]
 
             #Calculate alignment towards CoM
             r_dot_p = (-x_norm_unitv * px) + (-y_norm_unitv * py)
@@ -954,9 +955,7 @@ class particle_props:
                 elif self.typ[h]==1:
                     gas_A_val += (np.abs(vel[h])-gas_B_vel_avg)**2
 
-        print(gas_B_num)
-        print(gas_A_num)
-        stop
+
         # Average sum of bulk deviations divided by number of bulk particles of respective type ('all', 'A', or 'B')
         if bulk_num > 0:
             bulk_vel_std = (bulk_all_val/bulk_num)**0.5
@@ -1005,9 +1004,6 @@ class particle_props:
             gas_A_vel_std = 0
             gas_B_vel_std = 0
 
-        print(gas_B_vel_std)
-        print(gas_A_vel_std)
-        stop
 
         # Dictionary containing the average and standard deviation of velocity
         # of each phase and each respective type ('all', 'A', or 'B')
@@ -1516,11 +1512,9 @@ class particle_props:
 
         vel_A = vel_plot_dict['A']
         pos_A=self.pos[slow_ids]                               # Find positions of type 0 particles
-        ang_A=self.ang[slow_ids]
 
         vel_B = vel_plot_dict['B']
         pos_B=self.pos[fast_ids]                               # Find positions of type 0 particles
-        ang_B=self.ang[fast_ids]
 
         
         # Neighbor list query arguments to find interacting particles
@@ -1587,7 +1581,7 @@ class particle_props:
 
                     BB_neigh_ind = np.append(BB_neigh_ind, int(i))
                     
-                    BB_dot = np.append(BB_dot, np.sum(np.cos(ang_B[i]-ang_B[BB_nlist.point_indices[loc]])))
+                    BB_dot = np.append(BB_dot, np.sum((px_B[i]*px_B[BB_nlist.point_indices[loc]]+py_A[i]*py_B[BB_nlist.point_indices[loc]])/(((px_B[i]**2+py_B[i]**2)**0.5)*((px_B[BB_nlist.point_indices[loc]]**2+py_B[BB_nlist.point_indices[loc]]**2)**0.5))))
 
                     ang_rel = (vel_B['x'][i] * vel_B['x'][BB_nlist.point_indices[loc]] + vel_B['y'][i] * vel_B['y'][BB_nlist.point_indices[loc]])/(vel_B['mag'][i] * vel_B['mag'][BB_nlist.point_indices[loc]])
 
@@ -1660,7 +1654,7 @@ class particle_props:
 
                     AB_neigh_ind = np.append(AB_neigh_ind, int(i))
 
-                    AB_dot = np.append(AB_dot, np.sum(np.cos(ang_B[i]-ang_A[AB_nlist.point_indices[loc]])))
+                    AB_dot = np.append(AB_dot, np.sum((px_B[i]*px_A[AB_nlist.point_indices[loc]]+py_B[i]*py_A[AB_nlist.point_indices[loc]])/(((px_B[i]**2+py_B[i]**2)**0.5)*((px_A[AB_nlist.point_indices[loc]]**2+py_A[AB_nlist.point_indices[loc]]**2)**0.5))))
 
                     ang_rel = (vel_B['x'][i] * vel_A['x'][AB_nlist.point_indices[loc]] + vel_B['y'][i] * vel_A['y'][AB_nlist.point_indices[loc]])/(vel_B['mag'][i] * vel_A['mag'][AB_nlist.point_indices[loc]])
 
@@ -1734,7 +1728,7 @@ class particle_props:
 
                     BA_neigh_ind = np.append(BA_neigh_ind, int(i))
 
-                    BA_dot = np.append(BA_dot, np.sum(np.cos(ang_A[i]-ang_B[BA_nlist.point_indices[loc]])))
+                    BA_dot = np.append(BA_dot, np.sum((px_A[i]*px_B[BA_nlist.point_indices[loc]]+py_A[i]*py_B[BA_nlist.point_indices[loc]])/(((px_A[i]**2+py_A[i]**2)**0.5)*((px_B[BA_nlist.point_indices[loc]]**2+py_B[BA_nlist.point_indices[loc]]**2)**0.5))))
 
             else:
                 #Save nearest neighbor information to array
@@ -1804,7 +1798,7 @@ class particle_props:
 
                     AA_neigh_ind = np.append(AA_neigh_ind, int(i))
 
-                    AA_dot = np.append(AA_dot, np.sum(np.cos(ang_A[i]-ang_A[AA_nlist.point_indices[loc]])))
+                    AA_dot = np.append(AA_dot, np.sum((px_A[i]*px_A[AA_nlist.point_indices[loc]]+py_A[i]*py_A[AA_nlist.point_indices[loc]])/(((px_A[i]**2+py_A[i]**2)**0.5)*((px_A[AA_nlist.point_indices[loc]]**2+py_A[AA_nlist.point_indices[loc]]**2)**0.5))))
 
                     ang_rel = (vel_A['x'][i] * vel_A['x'][AA_nlist.point_indices[loc]] + vel_A['y'][i] * vel_A['y'][AA_nlist.point_indices[loc]])/(vel_A['mag'][i] * vel_A['mag'][AA_nlist.point_indices[loc]])
 
@@ -1893,12 +1887,10 @@ class particle_props:
         # Position and orientation arrays of type A particles in respective phase
         typ0ind = np.where(self.typ==0)[0]
         pos_A=self.pos[typ0ind]                               # Find positions of type 0 particles
-        ang_A=self.ang[typ0ind]
 
         # Position and orientation arrays of type B particles in respective phase
         typ1ind = np.where(self.typ==1)[0]
         pos_B=self.pos[typ1ind]
-        ang_B=self.ang[typ1ind]
 
         #Initiate empty arrays for calculating mean and standard deviation of local density
         #of all neighbor particles around A reference particles
@@ -2184,12 +2176,10 @@ class particle_props:
         # Position and orientation arrays of type A particles in respective phase
         typ0ind = np.where(self.typ==0)[0]
         pos_A=self.pos[typ0ind]                               # Find positions of type 0 particles
-        ang_A=self.ang[typ0ind]
 
         # Position and orientation arrays of type B particles in respective phase
         typ1ind = np.where(self.typ==1)[0]
         pos_B=self.pos[typ1ind]
-        ang_B=self.ang[typ1ind]
 
         #Initiate empty arrays for calculating mean and standard deviation of local density
         #of all neighbor particles around A reference particles
@@ -2888,10 +2878,7 @@ class particle_props:
             except:
                 com_x = all_surface_measurements[key]['interior']['com']['x']
                 com_y = all_surface_measurements[key]['interior']['com']['y']
-        
-            print(com_x)
-            print(com_y)
-        stop
+
         # Instantiate empty array (partNum) containing the average active force alignment
         # with the nearest interface surface normal
         part_align = method2_align_dict['part']['align']
@@ -3106,8 +3093,6 @@ class particle_props:
 
         int_ids = int_dict['bin']
         for m in range(0, len(sep_surface_dict)):
-            print('test')
-            print(m)
             key = 'surface id ' + str(int(int_comp_dict['ids'][m]))
             key2 = 'surface ' + str(int(int_comp_dict['ids'][m]))
 
@@ -3315,8 +3300,6 @@ class particle_props:
         com_radial_dict_fa = {}
         int_ids = int_dict['bin']
         for m in range(0, len(sep_surface_dict)):
-            print('test')
-            print(m)
             key = 'surface id ' + str(int(int_comp_dict['ids'][m]))
             key2 = 'surface ' + str(int(int_comp_dict['ids'][m]))
 
@@ -3640,8 +3623,6 @@ class particle_props:
         com_radial_dict_fa = {}
         int_ids = int_dict['bin']
         for m in range(0, len(sep_surface_dict)):
-            print('test')
-            print(m)
             key = 'surface id ' + str(int(int_comp_dict['ids'][m]))
             key2 = 'surface ' + str(int(int_comp_dict['ids'][m]))
 
@@ -4125,12 +4106,14 @@ class particle_props:
         # Position and orientation arrays of type A particles in respective phase
         typ0ind = np.where(self.typ==0)[0]
         pos_A=self.pos[typ0ind]                               # Find positions of type 0 particles
-        ang_A=self.ang[typ0ind]
+        px_A=self.px[typ0ind]
+        py_A=self.py[typ0ind]
 
         # Position and orientation arrays of type B particles in respective phase
         typ1ind = np.where(self.typ==1)[0]
         pos_B=self.pos[typ1ind]
-        ang_B=self.ang[typ1ind]
+        px_B=self.px[typ1ind]
+        py_B=self.py[typ1ind]
         
         # Neighbor list query arguments to find interacting particles
         query_args = dict(mode='ball', r_min = 0.1, r_max = self.r_cut)#r_max=self.theory_functs.conForRClust(peNet_int-45., self.eps) * 1.0)
@@ -4160,7 +4143,9 @@ class particle_props:
                     #Save nearest neighbor information to array
                     AA_num_neigh = np.append(AA_num_neigh, len(loc))
                     AA_neigh_ind = np.append(AA_neigh_ind, int(i))
-                    AA_dot = np.append(AA_dot, np.sum(np.cos(ang_A[i]-ang_A[AA_nlist.point_indices[loc]])))
+
+                    
+                    AA_dot = np.append(AA_dot, np.sum((px_A[i]*px_A[AA_nlist.point_indices[loc]]+py_A[i]*py_A[AA_nlist.point_indices[loc]])/(((px_A[i]**2+py_A[i]**2)**0.5)*((px_A[AA_nlist.point_indices[loc]]**2+py_A[AA_nlist.point_indices[loc]]**2)**0.5))))
             else:
                 #Save nearest neighbor information to array
                 AA_num_neigh = np.append(AA_num_neigh, 0)
@@ -4181,7 +4166,7 @@ class particle_props:
                     #Save nearest neighbor information to array
                     BA_num_neigh = np.append(BA_num_neigh, len(loc))
                     BA_neigh_ind = np.append(BA_neigh_ind, int(i))
-                    BA_dot = np.append(BA_dot, np.sum(np.cos(ang_A[i]-ang_B[BA_nlist.point_indices[loc]])))
+                    BA_dot = np.append(BA_dot, np.sum((px_A[i]*px_B[BA_nlist.point_indices[loc]]+py_A[i]*py_B[BA_nlist.point_indices[loc]])/(((px_A[i]**2+py_A[i]**2)**0.5)*((px_B[BA_nlist.point_indices[loc]]**2+py_B[BA_nlist.point_indices[loc]]**2)**0.5))))
             else:
                 #Save nearest neighbor information to array
                 BA_num_neigh = np.append(BA_num_neigh, 0)
@@ -4202,7 +4187,7 @@ class particle_props:
 
                     #Save nearest neighbor information to array
                     AB_num_neigh = np.append(AB_num_neigh, len(loc))
-                    AB_dot = np.append(AB_dot, np.sum(np.cos(ang_B[i]-ang_A[AB_nlist.point_indices[loc]])))
+                    AB_dot = np.append(AB_dot, np.sum((px_B[i]*px_A[AB_nlist.point_indices[loc]]+py_B[i]*py_A[AB_nlist.point_indices[loc]])/(((px_B[i]**2+py_B[i]**2)**0.5)*((px_A[AB_nlist.point_indices[loc]]**2+py_A[AB_nlist.point_indices[loc]]**2)**0.5))))
                     AB_neigh_ind = np.append(AB_neigh_ind, int(i))
             else:
                 #Save nearest neighbor information to array
@@ -4225,7 +4210,7 @@ class particle_props:
                     #Save nearest neighbor information to array
                     BB_num_neigh = np.append(BB_num_neigh, len(loc))
                     BB_neigh_ind = np.append(BB_neigh_ind, int(i))
-                    BB_dot = np.append(BB_dot, np.sum(np.cos(ang_B[i]-ang_B[BB_nlist.point_indices[loc]])))
+                    BB_dot = np.append(BB_dot, np.sum((px_B[i]*px_B[BB_nlist.point_indices[loc]]+py_B[i]*py_B[BB_nlist.point_indices[loc]])/(((px_B[i]**2+py_B[i]**2)**0.5)*((px_B[BB_nlist.point_indices[loc]]**2+py_B[BB_nlist.point_indices[loc]]**2)**0.5))))
             else:
                 #Save nearest neighbor information to array
                 BB_num_neigh = np.append(BB_num_neigh, 0)

post_proc/lib/plotting.py
@@ -298,7 +298,6 @@ class plotting:
         
         nonzero_x_id = nonzero_id[0]
         nonzero_y_id = nonzero_id[1]
-        print(active_fa_dict)
         # Plot averaged, binned orientation of particles
         """
         if orientation_id == True:
@@ -342,15 +341,13 @@ class plotting:
 
 
                     active_r = ( np.array(new_orient_dict_x) ** 2 + np.array(new_orient_dict_y) ** 2 ) ** 0.5
-                    print(active_r)
+
                     nonzero_id = np.nonzero(active_r)
                     
                     nonzero_x_id = nonzero_id[0]
                     nonzero_y_id = nonzero_id[1]
                     
                     for i in range(0, len(nonzero_x_id)):
-                        print('test')
-                        print(i)
                         #plt.quiver(self.pos_x[nonzero_x_id[i]][nonzero_y_id[i]]*3, self.pos_y[nonzero_x_id[i]][nonzero_y_id[i]]*3, active_fa_dict['bin']['x'][nonzero_x_id[i]][nonzero_y_id[i]], active_fa_dict['bin']['y'][nonzero_x_id[i]][nonzero_y_id[i]], scale=10.0, width=0.01, color='black', alpha=0.8)
                         plt.quiver(pos_x_new[nonzero_x_id[i]][nonzero_y_id[i]], pos_y_new[nonzero_x_id[i]][nonzero_y_id[i]], new_orient_dict_x[nonzero_x_id[i]][nonzero_y_id[i]], new_orient_dict_y[nonzero_x_id[i]][nonzero_y_id[i]], scale=10.0, width=0.01, color='black', alpha=0.8)
                     
@@ -1389,8 +1386,6 @@ class plotting:
         yellow = ("#7570b3")
         green = ("#77dd77")
 
-        print(prob_stat_dict)
-
         xmin = -0.5
         xmax=7.5
 
@@ -1551,8 +1546,6 @@ class plotting:
         #Define colors for plots
         yellow = ("#7570b3")
         green = ("#77dd77")
-
-        print(prob_stat_dict)
 
         xmin = -0.5
         xmax=7.5
@@ -2827,7 +2820,7 @@ class plotting:
         plt.savefig(self.outPath + 'csp_' + pair + '_' + self.outFile + ".png", dpi=150, transparent=False)
         plt.close() 
     
-    def plot_neighbors(self, neigh_plot_dict, ang, pos, pair='all-all', sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, interface_id = False, orientation_id = False):
+    def plot_neighbors(self, neigh_plot_dict, px, py, pos, pair='all-all', sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, interface_id = False, orientation_id = False):
         """
         This function plots the number of neighbors of all dense phase particles
         at each location in space.
@@ -2955,8 +2948,6 @@ class plotting:
             coll = ax.add_collection(neighborGroup)
             coll.set_array(np.ravel(num_neigh_arr))
 
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -4451,7 +4442,7 @@ class plotting:
         plt.savefig(self.outPath + 'clustering_'  + type + '_' + self.outFile + ".png", dpi=150, transparent=False)
         plt.close()  
         
-    def plot_neighbors2(self, neigh_plot_dict, ang, pos, prev_pos, sep_surface_dict=None, int_comp_dict=None, pair='all-all'):
+    def plot_neighbors2(self, neigh_plot_dict, px, py, pos, prev_pos, sep_surface_dict=None, int_comp_dict=None, pair='all-all'):
         """
         This function plots the number of neighbors of all dense phase particles
         at each location in space.
@@ -4570,9 +4561,6 @@ class plotting:
             neighborGroup = mc.PatchCollection(ells, cmap=plt.cm.get_cmap('tab10', int(max_neigh) + 1))#facecolors=slowCol)
             coll = ax.add_collection(neighborGroup)
             coll.set_array(np.ravel(neigh_plot_dict['all-all']['neigh']))
-
-            px = np.sin(ang[typ1ind])
-            py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box-mean_dify, px, py, color='black', width=0.003)
@@ -6301,9 +6289,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         coll = ax.add_collection(neighborGroup)
         coll.set_array(np.ravel(num_neigh_arr))
 
-        px = np.sin(ang[typ1ind])
-        py = -np.cos(ang[typ1ind])
-
         #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
         plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box-mean_dify, px, py, color='black', width=0.002)
         plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box+self.ly_box-mean_dify, px, py, color='black', width=0.002)
@@ -6505,8 +6490,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                 leg = ax0.legend(handles=[ells0[0]], labels=[r'$\mathrm{Pe}_\mathrm{A} = $'+str(int(self.peA)), r'$\mathrm{Pe}_\mathrm{A} = $'+str(int(self.peA))], loc='upper right', prop={'size': 15}, markerscale=8.0)
                 leg.legendHandles[0].set_color(slowCol)
             """
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
+
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #ax0.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -6641,7 +6625,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
 
         # Create frame pad for images
         #pad = str(j).zfill(4)
-        print(outfile_name)
 
         import pandas as pd
 
@@ -6738,7 +6721,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         plt.tight_layout()
         plt.savefig(self.outPath + 'clust_fluctuations_' + self.outFile + ".png", dpi=150, transparent=False)
         plt.close()   
-    def plot_part_activity_wide_desorb(self, pos, ang, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
+    def plot_part_activity_wide_desorb(self, pos, px, py, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
 
         """
         This function plots the particle positions and color codes each particle with its intrinsic
@@ -6913,9 +6896,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
             pos[:,1]= np.sin(np.pi/4) * pos_old[:,0] + np.cos(np.pi/4) * pos_old[:,1]
 
         sz = 0.755
-
-        px = np.cos(ang)
-        py = np.sin(ang)
 
         if mono==0:
 
@@ -7101,8 +7081,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                 one_leg = ax.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax.transAxes, bbox_to_anchor=[0.99, 1.19], handlelength=1.5, columnspacing=0.4, fontsize=36, ncol=2, facecolor='none', edgecolor='none')
                 ax.add_artist(one_leg)
             
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -7657,8 +7635,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                 one_leg = ax.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax.transAxes, bbox_to_anchor=[0.99, 1.19], handlelength=1.5, columnspacing=0.4, fontsize=36, ncol=2, facecolor='none', edgecolor='none')
                 ax.add_artist(one_leg)
             
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -7853,7 +7829,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         #plt.savefig(self.outPath + 'part_activity_' + self.outFile + ".eps", format='eps', dpi=150, bbox_inches='tight')
         plt.close()  
 
-    def plot_part_activity_wide_adsorb(self, pos, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
+    def plot_part_activity_wide_adsorb(self, pos, px, py, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None, mono_id=False, interface_id = False, orientation_id = False, zoom_id = False, banner_id = False, presentation_id = False):
 
         """
         This function plots the particle positions and color codes each particle with its intrinsic
@@ -8199,9 +8175,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                     fast_leg = [Line2D([0], [0], lw=0, marker='o', markeredgewidth=1.8*1.2, markeredgecolor='None', markerfacecolor=slowCol, label=r'$\mathrm{Pe}_\mathrm{S} = $'+str(int(self.peB)), markersize=32), Line2D([0], [0], lw=0, marker='o', markeredgewidth=1.8*1.2, markeredgecolor='None', markerfacecolor=fastCol, label=r'$\mathrm{Pe}_\mathrm{F} = $'+str(int(self.peA)), markersize=32)]
                 one_leg = ax.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax.transAxes, bbox_to_anchor=[0.99, 1.19], handlelength=1.5, columnspacing=0.4, fontsize=36, ncol=2, facecolor='none', edgecolor='none')
                 ax.add_artist(one_leg)
-            
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -8743,9 +8716,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                     fast_leg = [Line2D([0], [0], lw=0, marker='o', markeredgewidth=1.8*1.2, markeredgecolor='None', markerfacecolor=slowCol, label=r'$\mathrm{Pe}_\mathrm{S} = $'+str(int(self.peB)), markersize=32), Line2D([0], [0], lw=0, marker='o', markeredgewidth=1.8*1.2, markeredgecolor='None', markerfacecolor=fastCol, label=r'$\mathrm{Pe}_\mathrm{F} = $'+str(int(self.peA)), markersize=32)]
                 one_leg = ax.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax.transAxes, bbox_to_anchor=[0.99, 1.19], handlelength=1.5, columnspacing=0.4, fontsize=36, ncol=2, facecolor='none', edgecolor='none')
                 ax.add_artist(one_leg)
-            
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -9114,9 +9084,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                     fast_leg = [Line2D([0], [0], lw=0, marker='o', markeredgewidth=1.8*1.2, markeredgecolor='None', markerfacecolor=slowCol, label=r'$\mathrm{Pe}_\mathrm{S} = $'+str(int(self.peB)), markersize=32), Line2D([0], [0], lw=0, marker='o', markeredgewidth=1.8*1.2, markeredgecolor='None', markerfacecolor=fastCol, label=r'$\mathrm{Pe}_\mathrm{F} = $'+str(int(self.peA)), markersize=32)]
                 one_leg = ax.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax.transAxes, bbox_to_anchor=[0.99, 1.15], handlelength=1.5, columnspacing=0.4, fontsize=36, ncol=2, facecolor='none', edgecolor='none')
                 ax.add_artist(one_leg)
-            
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -9300,7 +9267,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         
 
         #com_opt = {'x': [np.mean(pos[bulk_part_ids,0])]+self.hx_box, 'y': [np.mean(pos[bulk_part_ids,1]+self.hy_box)]}
-        print(com_opt)
         typ0ind = np.where(self.typ == 0)[0]
         typ1ind = np.where(self.typ == 1)[0]
 
@@ -9485,8 +9451,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                 leg = ax.legend(handles=[ells0[0]], labels=[r'$\mathrm{Pe}_\mathrm{S} = $'+str(int(self.peA)), r'$\mathrm{Pe}_\mathrm{F} = $'+str(int(self.peA))], loc='upper right', prop={'size': 22}, markerscale=8.0, edgecolor='black', linewidth=2.0)
                 leg.legendHandles[0].set_color(slowCol)
             """
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -9818,8 +9782,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
                 leg = ax.legend(handles=[ells0[0]], labels=[r'$\mathrm{Pe}_\mathrm{S} = $'+str(int(self.peA)), r'$\mathrm{Pe}_\mathrm{F} = $'+str(int(self.peA))], loc='upper right', prop={'size': 22}, markerscale=8.0, edgecolor='black', linewidth=2.0)
                 leg.legendHandles[0].set_color(slowCol)
             """
-            #px = np.sin(ang[typ1ind])
-            #py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             #plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box, px, py, color='black', width=0.003)
@@ -9976,7 +9938,7 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
         #plt.savefig(self.outPath + 'part_activity_' + self.outFile + ".eps", format='eps', dpi=150, bbox_inches='tight')
         plt.close()
         
-    def plot_part_activity2(self, pos, prev_pos, ang, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None):
+    def plot_part_activity2(self, pos, prev_pos, px, py, sep_surface_dict=None, int_comp_dict=None, active_fa_dict=None):
 
         typ0ind = np.where(self.typ == 0)[0]
         typ1ind = np.where(self.typ == 1)[0]
@@ -10111,9 +10073,6 @@ values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, format=tick.FormatStr
             else:
                 leg = ax.legend(handles=[ells0[0]], labels=[r'$\mathrm{Pe}_\mathrm{A} = $'+str(int(self.peA)), r'$\mathrm{Pe}_\mathrm{A} = $'+str(int(self.peA))], loc='upper right', prop={'size': 15}, markerscale=8.0)
                 leg.legendHandles[0].set_color(slowCol)
-
-            px = np.sin(ang[typ1ind])
-            py = -np.cos(ang[typ1ind])
 
             #plt.scatter(neigh_plot_dict['all-all']['x'][typ1ind]+self.hx_box, neigh_plot_dict['all-all']['y'][typ1ind]+self.hy_box, c='black', s=sz)
             plt.quiver(pos[typ1ind,0]+self.hx_box, pos[typ1ind,1]+self.hy_box-mean_dify, px, py, color='black', width=0.003)

post_proc/lib/stress_and_pressure.py
@@ -42,7 +42,7 @@ import phase_identification
 import binning
 import particles
 class stress_and_pressure:
-    def __init__(self, lx_box, ly_box, NBins_x, NBins_y, partNum, phase_dict, pos, typ, ang, part_dict, eps, peA, peB, parFrac, align_dict, area_frac_dict, press_dict):
+    def __init__(self, lx_box, ly_box, NBins_x, NBins_y, partNum, phase_dict, pos, typ, px, py, part_dict, eps, peA, peB, parFrac, align_dict, area_frac_dict, press_dict):
 
         import freud
 
@@ -79,7 +79,8 @@ class stress_and_pressure:
 
         self.typ = typ
 
-        self.ang = ang
+        self.px = px
+        self.py = py
 
         self.binParts = part_dict['id']
 
@@ -99,13 +100,13 @@ class stress_and_pressure:
 
         self.press_dict = press_dict
 
-        self.binning = binning.binning(self.lx_box, self.ly_box, self.partNum, self.NBins_x, self.NBins_y, self.peA, self.peB, self.ang, self.eps)
+        self.binning = binning.binning(self.lx_box, self.ly_box, self.partNum, self.NBins_x, self.NBins_y, self.peA, self.peB, self.typ, self.eps)
 
         self.plotting_utility = plotting_utility.plotting_utility(self.lx_box, self.ly_box, self.partNum, self.typ)
 
         self.phase_ident = phase_identification.phase_identification(self.area_frac_dict, self.align_dict, self.part_dict, self.press_dict, self.lx_box, self.ly_box, self.partNum, self.NBins_x, self.NBins_y, self.peA, self.peB, self.parFrac, self.eps, self.typ)
 
-        self.particle_prop_functs = particles.particle_props(self.lx_box, self.ly_box, self.partNum, self.NBins_x, self.NBins_y, self.peA, self.peB, self.eps, self.typ, self.pos, self.ang)
+        self.particle_prop_functs = particles.particle_props(self.lx_box, self.ly_box, self.partNum, self.NBins_x, self.NBins_y, self.peA, self.peB, self.eps, self.typ, self.pos, self.px, self.py)
 
         self.theory_functs = theory.theory()
 
@@ -728,10 +729,6 @@ class stress_and_pressure:
                     interior_surface_arr = np.append(interior_surface_arr, sep_surface_dict[key]['interior']['mean radius'])
                 except:
                     interior_surface_arr = np.append(interior_surface_arr, 0)
-
-        print(exterior_surface_arr)
-        print(interior_surface_arr)
-        stop
 
         #Initiate empty values for integral of pressures across interfaces
         act_press = 0

post_proc/lib/utility.py
@@ -260,8 +260,8 @@ class utility:
         theta = -np.pi/2 + 2*np.arctan2(np.sqrt(1+2*(r*y-x*z)), np.sqrt(1-2*(r*y-x*z)))
         psi = np.arctan2(2*(r*z + x*y),1-2*(y**2+z**2))
         
-        x_vect = orientation_vector[0]
-        y_vect = orientation_vector[1]
+        x_vect = orientation_vector[0][0]
+        y_vect = orientation_vector[1][0]
         #sto
         
         
@@ -300,8 +300,8 @@ class utility:
 
         orientation_vector = np.matmul(rot_matrix, z_axis)
 
-        x_vect = orientation_vector[0]
-        y_vect = orientation_vector[1]
+        x_vect = orientation_vector[0][0]
+        y_vect = orientation_vector[1][0]
         
 
         return x_vect
@@ -329,8 +329,8 @@ class utility:
 
         orientation_vector = np.matmul(rot_matrix, z_axis)
 
-        x_vect = orientation_vector[0]
-        y_vect = orientation_vector[1]
+        x_vect = orientation_vector[0][0]
+        y_vect = orientation_vector[1][0]
         
 
         return y_vect