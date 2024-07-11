73c55092877c1e56d02f1859f473ea95d959e8fb
Nicholas Lauersdorf <njlauersdorf@wisc.edu>
2024-07-11 16:01:51 -0400

    fixed fluctuations plot

--- src/post_proc/plotting.py
+++ src/post_proc/plotting.py
@@ -8407,7 +8407,7 @@ class plotting:
                     one_leg = ax0.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax0.transAxes, bbox_to_anchor=[0.97, 1.17], handlelength=1.5, columnspacing=0.4, fontsize=26, ncol=2, facecolor='none', edgecolor='none')
                     ax0.add_artist(one_leg)
                 elif (self.peA<10):
-                    one_leg = ax0.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax0.transAxes, bbox_to_anchor=[1.04, 1.17], handlelength=1.5, columnspacing=0.4, fontsize=26, ncol=2, facecolor='none', edgecolor='none')
+                    one_leg = ax0.legend(handles=fast_leg, loc='upper right', borderpad=0.3, labelspacing=0.4, handletextpad=-0.2, bbox_transform=ax0.transAxes, bbox_to_anchor=[1.03, 1.17], handlelength=1.5, columnspacing=0.4, fontsize=26, ncol=2, facecolor='none', edgecolor='none')
                     ax0.add_artist(one_leg)
 
 
